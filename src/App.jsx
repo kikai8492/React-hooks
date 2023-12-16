c8e50452a1dc8b88f76e1b4366797f386be59a34
shuto kikai <yysskikai04@icloud.com>
2023-12-16 14:40:19 +0900

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>UseState, UseEffect</h1>
+      <button onClick={handleClick}>追加</button>
+      <p>{count}</p>
+    </div>
+  );
 }
 
 export default App;
